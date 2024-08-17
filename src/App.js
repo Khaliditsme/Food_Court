ad5ed2bedc67a97e0345166575b97f8fbc2af016
Khalid Meraj <124028350+Khaliditsme@users.noreply.github.com>
2024-08-17 20:18:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Body from './components/Body.js'
+import Header from './components/Header.js'
+import Footer from './components/Footer.js'
+import About from './components/About.js'
+import Contact from './components/Contact.js'
+import Error from './components/Error.js'
+import { Outlet } from "react-router-dom";
+import RestaurantDetails from "./components/RestaurantDetails.js";
+// import RestaurantMenu from "./components/RestaurantMenu.js";
+// import RestaurantInfo from "./components/RestaurantInfo.js";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Body />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="restaurant/:resId" element={<RestaurantDetails />} />
+        </Route>
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
